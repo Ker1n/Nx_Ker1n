f7fb8135c90ca455125874329b024559ed01f852
Markov Kirill <Ker1n.mkc@gmail.com>
2024-03-11 21:45:07 +0200

    init

---
+++ test lib/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { TestLibComponent } from './test lib/test lib.component';
+
+export const testLibRoutes: Route[] = [
+  { path: '', component: TestLibComponent },
+];
---
+++ test lib/src/lib/test lib/test lib.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ker1n-test-lib',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test lib.component.html',
+  styleUrl: './test lib.component.scss',
+})
+export class TestLibComponent {}
